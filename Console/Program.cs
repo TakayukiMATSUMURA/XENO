eb4452d834ce54f9cb9a9e3675cf37d3eaef8470
Takayuki Matsumura <matsumura@rounders.co.jp>
2023-02-25 01:52:39 +0900

    Consoleプロジェクトを.NET 7へ変更

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,49 +1,28 @@
 ﻿using System.Linq;
 using System.Collections.Generic;
+using XENO.Brains;
+using XENO;
 
-namespace XENO.Console
+var players = new List<Player>()
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var counter = new int[2];
-            for (var i = 0; i < 10; i++)
-            {
-                System.Console.WriteLine($"第{i + 1}ゲーム");
-                var players = new List<Player>()
-                {               
-                    new Player("Alice", new Brains.ConsoleInput()),
-                    new Player("Bob")
-                };
-                
-                var alice = players[0];
-                alice.OnReceive += (card) => Log.Output($"プレイヤー:{alice.ToString()}に{card.ToString()}が配られた.");
-                alice.OnAdd += (card) => Log.Output($"プレイヤー:{alice.ToString()}は{card.ToString()}を引いた.");
-                alice.OnSage += (cards) => Log.Output($"プレイヤー:{alice.ToString()}は賢者の効果で{string.Join(",", cards.Select(x => x.ToString()).ToArray())}を見た.");
-                alice.OnUse += (card) => Log.Output($"プレイヤー:{alice.ToString()}は{card}を使用.");
-                alice.OnDiscard += (card) => Log.Output($"プレイヤー:{alice.ToString()}は{card.ToString()}を捨てた.");
-                alice.OnRebirth += (card) => Log.Output($"{alice.ToString()}は{card.ToString()}へ転生した.");
+    new Player("Alice", new XENO.Brains.ConsoleInput()),
+    new Player("Bob")
+};
 
-                var deck = new Deck();
-                deck.Shuffle();
-                deck.SetRebirthCard();
-                foreach(var player in players)
-                {
-                    player.Receive(deck.Draw());
-                }
-                var game = new Game(players, deck);
-                game.Start();
-                if(game.GetWinners().Contains(players[0]))
-                {
-                    counter[0]++;
-                }
-                if(game.GetWinners().Contains(players[1]))
-                {
-                    counter[1]++;
-                }
-                Log.Output($"勝率:{(counter[0] * 100) / (i + 1)}% ({counter[0]}/{i + 1})\n");
-            }
-        }
-    }
+var alice = players[0];
+alice.OnReceive += (card) => Log.Output($"プレイヤー:{alice.ToString()}に{card.ToString()}が配られた.");
+alice.OnAdd += (card) => Log.Output($"プレイヤー:{alice.ToString()}は{card.ToString()}を引いた.");
+alice.OnSage += (cards) => Log.Output($"プレイヤー:{alice.ToString()}は賢者の効果で{string.Join(",", cards.Select(x => x.ToString()).ToArray())}を見た.");
+alice.OnUse += (card) => Log.Output($"プレイヤー:{alice.ToString()}は{card}を使用.");
+alice.OnDiscard += (card) => Log.Output($"プレイヤー:{alice.ToString()}は{card.ToString()}を捨てた.");
+alice.OnRebirth += (card) => Log.Output($"{alice.ToString()}は{card.ToString()}へ転生した.");
+
+var deck = new Deck();
+deck.Shuffle();
+deck.SetRebirthCard();
+foreach (var player in players)
+{
+    player.Receive(deck.Draw());
 }
+var game = new Game(players, deck);
+game.Start();
